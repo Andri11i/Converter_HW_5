85e1bafdf76baecf71fd06a58827206a0e1b3b7e
Andri11i <zotov.andrey.2008@gmail.com>
2024-11-26 11:57:02 +0200

    Merge

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,13 +6,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Converter App.");
 
+        System.out.println("App for measures converting.");
 
-        System.out.println("Version 1.0");
-        double pnds = 12;
         double kgs = 5;
         double pounds = convKgsToPounds(kgs);
-        double kilos = convPoundsToKgs(pnds);
-        System.out.println("Result is " + pounds + " pounds and kilos is " + kilos);
+
+        System.out.println("Result is " + pounds + " pounds.");
 
     }
 
@@ -20,8 +19,5 @@ public class Main {
         return kgs * CONV_K;
     }
 
-    private static double convPoundsToKgs(double pounds) {
-        return pounds / CONV_K;
-    }
 
 }
